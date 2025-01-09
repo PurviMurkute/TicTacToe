d80207bcb09617b6f262eab0f848d47b220dbae4
PurviMurkute <purvimurkute51@gmail.com>
2025-01-09 17:10:30 +0530

    game modified

--- ttt.js
+++ ttt.js
@@ -1,125 +1,92 @@
 const board = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
 let currentPlayer = "X";
-function renderBoard(){
-    const allBoxes = document.querySelectorAll('.box');
+function renderBoard() {
+  const allBoxes = document.querySelectorAll(".box");
 
-    allBoxes.forEach((boxElement, i)=>{
-        if(board[i]==='X'){
-            boxElement.innerHTML = '<img src="crossicon.png" height="40px"/>';
-        }
-        else if(board[i]==='O'){
-            boxElement.innerHTML = '<img src="circleicon.png" height="40px"/>';
-        }
-        else{
-            boxElement.innerHTML = ' ';
-        }
-    })
+  allBoxes.forEach((boxElement, i) => {
+    if (board[i] === "X") {
+      boxElement.innerHTML = '<img src="crossicon.png" height="40px"/>';
+    } else if (board[i] === "O") {
+      boxElement.innerHTML = '<img src="circleicon.png" height="40px"/>';
+    } else {
+      boxElement.innerHTML = " ";
+    }
+  });
 }
 renderBoard();
 
-function selectBox(boxNum){
-    if(board[boxNum] !== -1){
-        alert("Invalid Move");
-        return;
-    }
-    board[boxNum] = currentPlayer;
+function selectBox(boxNum) {
+  if (board[boxNum] !== -1) {
+    alert("Invalid Move");
+    return;
+  }
+  board[boxNum] = currentPlayer;
 
-    renderBoard();
+  renderBoard();
 
-    if(currentPlayer === "X"){
-        currentPlayer = "O";
-    }
-    else{
-        currentPlayer = "X";
-    }
-
-    const currentPlayerElement = document.getElementById("currentPlayer");
-    currentPlayerElement.innerText = currentPlayer;
-
-    checkforWinner();
-}
-
-function resetGame(){
-    // board.forEach((box, i)=>{
-    //     board[i] = -1;
-    // })
-    board.fill(-1);
-    renderBoard();
-
+  if (currentPlayer === "X") {
+    currentPlayer = "O";
+  } else {
     currentPlayer = "X";
-    const currentPlayerElement = document.getElementById("currentPlayer");
-    currentPlayerElement.innerText = currentPlayer;
+  }
+
+  const currentPlayerElement = document.getElementById("currentPlayer");
+  currentPlayerElement.innerText = currentPlayer;
+
+  checkforWinner();
 }
 
-function presentPlayer(places, player){
-    let result = true;
-    places.forEach((place)=>{
-         if(board[place]!=player){
-            result = false;
-         }
-    })
-    return result;
+function resetGame() {
+  // board.forEach((box, i)=>{
+  //     board[i] = -1;
+  // })
+  board.fill(-1);
+  renderBoard();
+
+  currentPlayer = "X";
+  const currentPlayerElement = document.getElementById("currentPlayer");
+  currentPlayerElement.innerText = currentPlayer;
+
+  const winnerMessage = document.getElementById("winnerMsg");
+  winnerMessage.innerHTML = ""; 
 }
 
-function checkforWinner(){
-    // winning positions:
-    // [0, 1, 2],
-    // [3, 4, 5],
-    // [6, 7, 8],
-    // [0, 3, 6],
-    // [1, 4, 7],
-    // [2, 5, 8],
-    // [0, 4, 8],
-    // [2, 4, 6]
-    if(presentPlayer([0, 1, 2], "X")){
-        alert("Player X Wins");
-        
-        places.innerHTML = '<img src="greencross.png"/>';
-    }else if(presentPlayer([3, 4, 5], "X")){
-        alert("Player X Wins");
-        places.innerHTML = '<img src="greencross.png"/>';
-    }else if(presentPlayer([6, 7, 8], "X")){
-        alert("Player X Wins");
-        places.innerHTML = '<img src="greencross.png"/>';
-    }else if(presentPlayer([0, 3, 6], "X")){
-        alert("Player X Wins");
-        places.innerHTML = '<img src="greencross.png"/>';
-    }else if(presentPlayer([1, 4, 7], "X")){
-        alert("Player X Wins");
-        places.innerHTML = '<img src="greencross.png"/>';
-    }else if(presentPlayer([2, 5, 8], "X")){
-        alert("Player X Wins");
-        places.innerHTML = '<img src="greencross.png"/>';
-    }else if(presentPlayer([0, 4, 8], "X")){
-        alert("Player X Wins");
-        places.innerHTML = '<img src="greencross.png"/>';
-    }else if(presentPlayer([2, 4, 6], "X")){
-        alert("Player X Wins");
-        places.innerHTML = '<img src="greencross.png"/>';
+function presentPlayer(places, player) {
+  let result = true;
+  places.forEach((place) => {
+    if (board[place] != player) {
+      result = false;
     }
-    if(presentPlayer([0, 1, 2], "O")){
-        alert("Player O Wins");
-        places.innerHTML = '<img src="greencircle.png"/>';
-    }else if(presentPlayer([3, 4, 5], "O")){
-        alert("Player O Wins");
-        places.innerHTML = '<img src="greencircle.png"/>';
-    }else if(presentPlayer([6, 7, 8], "O")){
-        alert("Player O Wins");
-        places.innerHTML = '<img src="greencircle.png"/>';
-    }else if(presentPlayer([0, 3, 6], "O")){
-        alert("Player O Wins");
-        places.innerHTML = '<img src="greencircle.png"/>';
-    }else if(presentPlayer([1, 4, 7], "O")){
-        alert("Player O Wins");
-        places.innerHTML = '<img src="greencircle.png"/>';
-    }else if(presentPlayer([2, 5, 8], "O")){
-        alert("Player O Wins");
-        places.innerHTML = '<img src="greencircle.png"/>';
-    }else if(presentPlayer([0, 4, 8], "O")){
-        alert("Player O Wins");
-        places.innerHTML = '<img src="greencircle.png"/>';
-    }else if(presentPlayer([2, 4, 6], "O")){
-        alert("Player O Wins");
-        places.innerHTML = '<img src="greencircle.png"/>';
-    }
+  });
+  return result;
+}
+
+function checkforWinner() {
+  const winnerMessage = document.getElementById("winnerMsg");
+
+  if (
+    presentPlayer([0, 1, 2], "X") ||
+    presentPlayer([3, 4, 5], "X") ||
+    presentPlayer([6, 7, 8], "X") ||
+    presentPlayer([0, 3, 6], "X") ||
+    presentPlayer([1, 4, 7], "X") ||
+    presentPlayer([2, 5, 8], "X") ||
+    presentPlayer([0, 4, 8], "X") ||
+    presentPlayer([2, 4, 6], "X")
+  ) {
+    winnerMessage.innerHTML = `<p>Player X Wins!</p>`;
+  } else if (
+    presentPlayer([0, 1, 2], "O") ||
+    presentPlayer([3, 4, 5], "O") ||
+    presentPlayer([6, 7, 8], "O") ||
+    presentPlayer([0, 3, 6], "O") ||
+    presentPlayer([1, 4, 7], "O") ||
+    presentPlayer([2, 5, 8], "O") ||
+    presentPlayer([0, 4, 8], "O") ||
+    presentPlayer([2, 4, 6], "O")
+  ) {
+    winnerMessage.innerHTML = `<p>Player O Wins!</p>`;
+  } else {
+    winnerMessage.innerHTML = ""; // Clear the message if no winner yet
+  }
 }
